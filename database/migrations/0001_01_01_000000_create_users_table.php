6b3b4db3e80f53490c1711ef6ec3ef953802be00
fajrCode <fajri42vandi@gmail.com>
2024-06-07 21:49:35 +0700

    feat: auth | basic setup laravel breeze

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('name');
+            $table->string('jenis_kelamin');
+            $table->string('no_hp');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
