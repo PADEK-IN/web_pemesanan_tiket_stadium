0de0f6a32c2b8b7a25c1e80e22267ff22a7cbbab
fajrCode <fajri42vandi@gmail.com>
2024-06-11 11:24:44 +0700

    middlaware: edit default middleware | guest for direct if auth, auth login/register

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,13 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user = Auth::user();
+
+        if ($user->role==="admin") {
+            return redirect()->intended(route('admin.dashboard', absolute: false));
+        } elseif ($user->role==="user") {
+            return redirect()->intended(route('dashboard', absolute: false));
+        }
     }
 
     /**
