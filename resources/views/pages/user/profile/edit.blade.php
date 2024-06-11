96c4261d520b38c5d9bb921678bd9150722c59d3
fajrCode <fajri42vandi@gmail.com>
2024-06-11 11:26:57 +0700

    feat: User Page | Add all Pages Basic

---
+++ resources/views/pages/user/profile/edit.blade.php
@@ -0,0 +1,19 @@
+<x-user-layout>
+    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            @include('pages.admin.profile.partials.update-profile-information-form')
+        </div>
+    </div>
+
+    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            @include('pages.admin.profile.partials.update-password-form')
+        </div>
+    </div>
+
+    <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="max-w-xl">
+            @include('pages.admin.profile.partials.delete-user-form')
+        </div>
+    </div>
+</x-user-layout>
