614860010f9b7ebd84b26ccb7ec2f4dfc6e24cbf
fajrCode <fajri42vandi@gmail.com>
2024-06-12 09:37:24 +0700

    factories: set factories Event

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->jobTitle(),
+            'description' => fake()->paragraph(),
+            'image' => fake()->image(),
+            'date' => fake()->date(),
+            'time' => fake()->time(),
+        ];
+    }
+}
