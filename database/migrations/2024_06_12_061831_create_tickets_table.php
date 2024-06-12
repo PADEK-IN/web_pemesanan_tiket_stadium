dbb18f356abd6b0c36f0bfb9dcdcf30770af43b9
fajrCode <fajri42vandi@gmail.com>
2024-06-12 14:01:21 +0700

    new table: create migration and model ticket with relation to event

---
+++ database/migrations/2024_06_12_061831_create_tickets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('id_event')->constrained(table: 'events', indexName:'tickets_id_event');
+            $table->enum('ticket_type', ['reguler', 'vip']);
+            $table->decimal('price',10,2);
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
