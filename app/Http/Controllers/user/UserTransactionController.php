cdd86ac0f35c6fbc2ceca0f03d64a716975c60f0
fajrCode <fajri42vandi@gmail.com>
2024-06-16 15:31:42 +0700

    update: controller transaction user | add function create

--- app/Http/Controllers/user/UserTransactionController.php
+++ app/Http/Controllers/user/UserTransactionController.php
@@ -35,6 +35,48 @@ class UserTransactionController extends Controller
         return view('pages.user.transactions.create', compact('event', 'idUser'));
     }
 
+    public function createTransaction(Request $request): RedirectResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'id_event' => 'required|integer',
+            'quantity' => 'required|integer',
+            'proof' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // max 2MB
+        ]);
 
+        if ($validator->fails()) {
+            return redirect()->back()
+                            ->withErrors($validator)
+                            ->withInput();
+        }
+
+        // Handle image upload
+        if ($request->hasFile('proof')) {
+            $image = $request->file('proof');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $image->storeAs('/event', $imageName, 'public_custom');
+            $imagePath = 'assets/img/event/'.$imageName;
+        } else {
+            $imagePath = null; // Default image if no image uploaded
+        }
+
+        try {
+            $idUser = Auth::id();
+            $idEvent=Hashids::decode($request->input('id_event'));
+
+            Transaction::create([
+                'id_user' => $idUser,
+                'id_event' => $idEvent[0],
+                'quantity' => $request->input('rating'),
+                'proof' => $imagePath,
+            ]);
+
+        } catch (\Exception $e) {
+            return redirect()->back()
+                            ->with('error', 'Server error, gagal melakukan pemesanan. Harap coba lagi.')
+                            ->withInput();
+        }
+
+        return redirect()->route('transaction')->with('success', 'Berhasil melakukan pemesanan. Harap tunggu validasi 1x24 jam ya.');
+    }
 
 }
