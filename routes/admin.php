a235b0d67e9915f3afc0306d09701665deec997b
fajrCode <fajri42vandi@gmail.com>
2024-06-17 18:09:18 +0700

    refactor: routes file | split route admin and user

---
+++ routes/admin.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\EventController;
+use App\Http\Controllers\admin\IndexController;
+use App\Http\Controllers\admin\ProfileController;
+use App\Http\Controllers\admin\ReviewController;
+use App\Http\Controllers\admin\TransactionController;
+use App\Http\Controllers\admin\UserController;
+use Illuminate\Support\Facades\Route;
+
+// Admin Routes
+Route::prefix("admin")->middleware(["auth", "verified"])->group(function (){
+
+    Route::get('/dashboard', [IndexController::class, 'index'])->name('admin.dashboard');
+
+    Route::get('/event', [EventController::class, 'getAllData'])->name('admin.event');
+    Route::get('/event/create', [EventController::class, 'getCreatePage'])->name('admin.event.create');
+    Route::get('/event/{id}', [EventController::class, 'show'])->name('admin.event.show');
+    Route::post('/event/create', [EventController::class, 'create']);
+
+    Route::get('/category', [CategoryController::class, 'getAllData'])->name('admin.category');
+    Route::get('/category/create', [CategoryController::class, 'getCreatePage'])->name('admin.category.create');
+    Route::post('/category/create', [CategoryController::class, 'create'])->name('admin.category.create');
+
+    Route::get('/transaction', [TransactionController::class, 'getAllData'])->name('admin.transaction');
+    Route::get('/transaction/check/{id}', [TransactionController::class, 'detail'])->name('admin.transaction.detail');
+
+    Route::get('/review', [ReviewController::class, 'getAllData'])->name('admin.review');
+
+    Route::get('/user', [UserController::class, 'getAllData'])->name('admin.user');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
+
+});
--- routes/web.php
+++ routes/web.php
@@ -18,50 +18,6 @@ Route::get('/', function () {
     return view('pages.welcome');
 });
 
-// User Routes
-Route::middleware(["auth", "verified"])->group(function (){
-
-    Route::get('/dashboard', [UserIndexController::class, 'index'])->name('dashboard');
-
-    Route::get('/event', [UserEventController::class, 'getAllData'])->name('event');
-    Route::get('/event/{id}', [UserEventController::class, 'detail'])->name('event.detail');
-    Route::get('/event/review/{id}', [UserEventController::class, 'reviewPage'])->name('event.review');
-    Route::post('/event/review', [UserEventController::class, 'createReview'])->name('event.review.store');
-
-    Route::get('/transaction', [UserTransactionController::class, 'getAllData'])->name('transaction');
-    Route::get('/transaction/create/{id}', [UserTransactionController::class, 'createPage'])->name('transaction.create');
-    Route::post('/transaction/create/', [UserTransactionController::class, 'createTransaction'])->name('transaction.create.store');
-
-    Route::get('/profile', [UserProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [UserProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [UserProfileController::class, 'destroy'])->name('profile.destroy');
-
-});
-
-// Admin Routes
-Route::prefix("admin")->middleware(["auth", "verified"])->group(function (){
-
-    Route::get('/dashboard', [IndexController::class, 'index'])->name('admin.dashboard');
-
-    Route::get('/event', [EventController::class, 'getAllData'])->name('admin.event');
-    Route::get('/event/create', [EventController::class, 'getCreatePage'])->name('admin.event.create');
-    Route::get('/event/{id}', [EventController::class, 'show'])->name('admin.event.show');
-    Route::post('/event/create', [EventController::class, 'create']);
-
-    Route::get('/category', [CategoryController::class, 'getAllData'])->name('admin.category');
-    Route::get('/category/create', [CategoryController::class, 'getCreatePage'])->name('admin.category.create');
-    Route::post('/category/create', [CategoryController::class, 'create'])->name('admin.category.create');
-
-    Route::get('/transaction', [TransactionController::class, 'getAllData'])->name('admin.transaction');
-
-    Route::get('/review', [ReviewController::class, 'getAllData'])->name('admin.review');
-
-    Route::get('/user', [UserController::class, 'getAllData'])->name('admin.user');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('admin.profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
-
-});
-
 require __DIR__.'/auth.php';
+require __DIR__.'/admin.php';
+require __DIR__.'/user.php';
