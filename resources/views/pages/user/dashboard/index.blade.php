fce14df5352cdd2de85c309e494bbebe353086bf
fajrCode <fajri42vandi@gmail.com>
2024-06-17 17:10:34 +0700

    fix: dashboard user

--- resources/views/pages/user/dashboard/index.blade.php
+++ resources/views/pages/user/dashboard/index.blade.php
@@ -1,27 +1,11 @@
 <x-user-layout>
     <div class="row">
-        <!-- four widgets   -->
-        <!-- total views   -->
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+        <!-- total event   -->
+        <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
                 <div class="card-body">
                     <div class="d-inline-block">
-                        <h5 class="text-muted">Total Pengguna</h5>
-                        <h3 class="mb-0">{{ $qty['user'] }}</h3>
-                    </div>
-                    <div class="float-right icon-circle-medium  icon-box-lg  bg-info-light mt-1">
-                        <i class="fa fa-user fa-fw fa-sm text-info"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end total views   -->
-        <!-- total followers   -->
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-inline-block">
-                        <h5 class="text-muted">Total Acara</h5>
+                        <h5 class="text-muted">Total Acara Diikuti</h5>
                         <h3 class="mb-0">{{ $qty['event'] }}</h3>
                     </div>
                     <div class="float-right icon-circle-medium  icon-box-lg  bg-primary-light mt-1">
@@ -30,13 +14,13 @@
                 </div>
             </div>
         </div>
-        <!-- end total followers   -->
-        <!-- partnerships   -->
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+        <!-- end total event   -->
+        <!-- review   -->
+        <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
                 <div class="card-body">
                     <div class="d-inline-block">
-                        <h5 class="text-muted">Total Reviews</h5>
+                        <h5 class="text-muted">Total Ulasan Acara</h5>
                         <h3 class="mb-0">{{ $qty['review'] }}</h3>
                     </div>
                     <div class="float-right icon-circle-medium  icon-box-lg  bg-secondary-light mt-1">
@@ -45,9 +29,9 @@
                 </div>
             </div>
         </div>
-        <!-- end partnerships   -->
-        <!-- total earned   -->
-        <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+        <!-- end review   -->
+        <!-- total pembelian   -->
+        <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12">
             <div class="card">
                 <div class="card-body">
                     <div class="d-inline-block">
@@ -60,7 +44,7 @@
                 </div>
             </div>
         </div>
-        <!-- end total earned   -->
+        <!-- end total pembelian   -->
 
     </div>
 </x-user-layout>
