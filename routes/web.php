e06ccbaf89ba11352509f7df30116a6cf06e27dc
fajrCode <fajri42vandi@gmail.com>
2024-06-17 22:52:57 +0700

    fix: route web | clean code

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-use App\Http\Controllers\admin\CategoryController;
-use App\Http\Controllers\admin\EventController;
-use App\Http\Controllers\admin\IndexController;
-use App\Http\Controllers\admin\ProfileController;
-use App\Http\Controllers\admin\ReviewController;
-use App\Http\Controllers\admin\TransactionController;
-use App\Http\Controllers\admin\UserController;
-use App\Http\Controllers\user\UserEventController;
-use App\Http\Controllers\user\UserIndexController;
-use App\Http\Controllers\user\UserProfileController;
-use App\Http\Controllers\user\UserTransactionController;
 use Illuminate\Support\Facades\Route;
 
 // Guest Routes
